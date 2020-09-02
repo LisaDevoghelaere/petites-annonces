f4c87c37b9b0de0fcb98cc3b714b6081146b851c
sawkea <shoareau@orange.fr>
2020-09-02 15:32:48 +0200

    Updates création de la page ajout et test en cours

--- application/class/Annonce.php
+++ application/class/Annonce.php
@@ -2,57 +2,6 @@
 
 namespace App;
 
-
-// class Annonce{
-//     public static function pageAnnonce(){
-
-//         $annonce = self::voir_annonce();
-    
-        
-//         // echo"<pre>";
-//         // print_r ($annonce);
-//         // echo"</pre>";
-//         // die();
-//         $loader = new \Twig\Loader\FilesystemLoader('../application/template');
-//         $twig = new \Twig\Environment($loader, [
-//             'cache'=> '../application/cache',
-//             'debug' => true,
-            
-//         ]);
-       
-//         $twig->addExtension(new \Twig\Extension\DebugExtension());
-//         // self pour accéder aux propriétés ou méthodes de la classe
-        
-        
-//         // render template
-//         $template = $twig->load('annonce.html.twig');
-//         echo $template->render(array(
-//             'annonce' => $annonce,
-//             'ann_unique_id' => $ann_unique_id,
-//             'ann_description' => $ann_description
-//         ));
-        
-//     }
-
-//     private static function voir_annonce(){
-//         //Connexion
-//         $base = new \App\Db();
-        
-//         if(isset($_GET['ann_unique_id']) && !empty($_GET['ann_unique_id'])){
-//             $ann_unique_id = $_POST['ann_unique_id'];
-//         $sql = "SELECT ann_unique_id, ann_titre, ann_description, ann_prix, ann_image_url, ann_date_validation, usr_nom, cat_libelle FROM annonce INNER JOIN categorie ON categorie_id = cat_id INNER JOIN utilisateur ON id = utilisateur_id where ann_unique_id = :ann_unique_id";
-//         $data = $base->q($sql);  
-        
-//         return $data;
-
-
-//         }else{
-//             echo "erreur";
-//         }
-        
-//     }
-// }
-
 class Annonce
 {
     public $data=[];
@@ -88,11 +37,11 @@ class Annonce
         //Connexion
         $base = new \App\Db();
 
-        //Test if unique_id exist
-        $req = $base->q("SELECT `is_validated` FROM `annonce` WHERE `ann_unique_id` = :ann_unique_id",
+        //Test if ann_unique_id exist
+        $req = $base->q("SELECT `ann__est_valide` FROM `annonce` WHERE `ann_unique_id` = :ann_unique_id",
             array(array('ann_unique_id',$ann_unique_id,\PDO::PARAM_STR)));
 
-        $valid = $req[0]->is_validated;
+        $valid = $req[0]->ann__est_valide;
 
         if($valid === 1){
             return true;
@@ -101,11 +50,3 @@ class Annonce
     }
 
 }
-
-
-
-
-
-
-
-
--- application/class/ajout.php
+++ application/class/ajout.php
@@ -1,39 +1,159 @@
 <?php
 
 namespace App;
-
+use App\Annonce;
 
 class Ajout{
-    public static function pageAjout(){
-
-        $annonce = self::ajout_annonce();
-        $ann_unique_id = ':ann_unique_id';
+    public $data=[];
+    public static function Add(){
         
-        $loader = new \Twig\Loader\FilesystemLoader('../application/template');
-        $twig = new \Twig\Environment($loader, [
-            'cache'=> '../application/cache',
-            'debug' => true,
-        ]);
-        $twig->addExtension(new \Twig\Extension\DebugExtension());
+        $validation= (new self)->Validation();
+        echo 'coucou';
+        if($validation ==="OK"){
+            
+            // les valeurs
+            $ann_titre = $_POST['ann_titre'];
+            $categorie  = $_POST['categorie'];
+            $ann_description  = $_POST['ann_description'];
+            $ann_prix  = $_POST['ann_prix'];
+            $usr_nom  = $_POST['usr_nom'];
+            $usr_prenom  = $_POST['usr_prenom'];
+            $usr_courriel  = $_POST['usr_courriel'];
+            $usr_telephone  = $_POST['usr_telephone'];
 
-        
-        // render template
-        $template = $twig->load('ajout.html.twig');
-        echo $template->render(array(
-            'annonce' => $annonce,
-            'ann_unique_id' => $ann_unique_id
-        ));
-    
+            //Connexion
+            $base = new \App\Db();
+
+            //avoir la id de la categorie
+            $req = $base->q("SELECT `id` FROM `categorie` WHERE `cat_libelle` = :categorie",
+            array(array('categorie',$categorie,\PDO::PARAM_STR)));
+            $cat_id = $req[0]->id;
+
+            // définir dernier id
+            $req = $base->q("SELECT MAX(id) FROM `annonce`");
+            $id = $req[0]->id;
+            $id += 1;
+
+
+            // date_creation
+            $ann_date_ecriture = date("Y-m-d");
+
+            //ADD POST
+            // $base->q('INSERT INTO annonce(`ann_unique_id`, `ann_titre`, `cat_id`, `ann_description`, `ann_prix`, `usr_nom`, `usr_prenom`, `usr_courriel`, `usr_telephone`, `ann_date_ecriture`, ann_image_url)
+            //           VALUES (:ann_unique_id, :ann_titre, :categorie, :ann_description, :ann_prix, :usr_nom, :usr_prenom, :usr_courriel, :usr_telephone, :ann_date_ecriture, :ann_image_url)',
+            array(
+                array('ann_unique_id',$ann_unique_id,\PDO::PARAM_STR),
+                array('ann_titre',$ann_titre,\PDO::PARAM_STR),
+                array('cat_id',$cat_id,\PDO::PARAM_STR),
+                array('ann_description',$ann_description,\PDO::PARAM_STR),
+                array('ann_prix',$ann_prix,\PDO::PARAM_STR),
+                array('usr_nom',$usr_nom,\PDO::PARAM_INT),
+                array('usr_prenom',$usr_prenom,\PDO::PARAM_STR),
+                array('usr_courriel',$usr_courriel,\PDO::PARAM_STR),
+                array('usr_telephone',$usr_telephone,\PDO::PARAM_STR),
+                array('ann_date_ecriture',$ann_date_ecriture,\PDO::PARAM_STR),
+                array('ann_image_url',$ann_image_url,\PDO::PARAM_STR)
+                )
+            );
+        }
+    }
+    // Fonction validation d'une annonce
+    public function Validation(){
+        if(isset($_POST['ann_titre']) && !empty($_POST['ann_titre'])){
+            if(v::stringVal()->validate($_POST['ann_titre']) == false){
+                return 'Titre invalide';
+            }
+        }else{
+            return 'Veuillez entrer un titre';
+        }
+
+        if(isset($_POST['ann_description']) && !empty($_POST['ann_description'])){
+            if(v::stringVal()->validate($_POST['ann_description']) == false){
+                return 'Description invalide';
+            }
+        }else{
+            return 'Veuillez entrer une description';
+        }
+
+        if(isset($_POST['usr_courriel']) && !empty($_POST['usr_courriel'])){
+            if(v::email()->validate($_POST['user_mail']) == false){
+                return 'Adresse mail invalide';
+            }
+        }else{
+            return 'Veuillez entrer une adresse mail';
+        }
+
+        if(isset($_POST['usr_nom']) && !empty($_POST['usr_nom'])){
+            if(v::stringVal()->validate($_POST['usr_nom']) == false){
+                return 'Nom invalide';
+            }
+        }else{
+            return 'Veuillez entrer votre nom';
+        }
+
+        if(isset($_POST['usr_prenom']) && !empty($_POST['usr_prenom'])){
+            if(v::stringVal()->validate($_POST['usr_prenom']) == false){
+                return 'Prénom invalide';
+            }
+        }else{
+            return 'Veuillez entrer votre prénom';
+        }
+
+        if(isset($_POST['usr_telephone']) && !empty($_POST['usr_telephone'])){
+            if(v::stringVal()->validate($_POST['usr_telephone']) == false){
+                return 'Téléphone invalide';
+            }
+        }else{
+            return 'Veuillez entrer votre numero de téléphone';
+        }
+
+        if(isset($_POST['categorie']) && !empty($_POST['categorie'])){
+            if(v::stringVal()->validate($_POST['categorie']) == false){
+                return 'Catégorie invalide';
+            }
+        }else{
+            return 'Veuillez choisir une catégorie';
+        }
+        return "OK";
     }
 
-    private static function ajout_annonce(){
-        //Connexion
-        $base = new \App\Db();
-        
-        
-        $sql = "INSERT INTO annonce (ann_unique_id, ann_titre, ann_description, ann_prix, `ann_date_ecriture`, ann_image_url, cat_libelle, usr_nom, usr_prenom, usr_courriel INNER JOIN categorie ON categorie_id = cat_id INNER JOIN utilisateur ON id = utilisateur_id VALUES (:ann_unique_id, :ann_titre, :ann_description, :ann_prix, :ann_date_ecriture, :ann_image_url, :cat_libelle, :usr_nom, :usr_prenom, :usr_courriel)";
-        $data = $base->q($sql);  
-        return $data;
+    // Fonction télécharger une photo dans l'annonce
+    public function telecharger_photo(){
+        // Vérifier si le formulaire a été soumis
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            // Vérifie si le fichier a été uploadé sans erreur.
+            if(isset($_FILES["ann_image_url"]) && $_FILES["ann_image_url"]["error"] == 0){
+                $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+                $filename = $_FILES["ann_image_url"]["name"];
+                $filetype = $_FILES["ann_image_url"]["type"];
+                $filesize = $_FILES["ann_image_url"]["size"];
+
+                // Vérifie l'extension du fichier
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
+
+                // Vérifie la taille du fichier - 5Mo maximum
+                $maxsize = 5 * 1024 * 1024;
+                if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
+
+                // Vérifie le type MIME du fichier
+                if(in_array($filetype, $allowed)){
+                    // Vérifie si le fichier existe avant de le télécharger.
+                    if(file_exists("media/annonce/" . $_FILES["ann_image_url"]["name"])){
+                        echo $_FILES["ann_image_url"]["name"] . " existe déjà.";
+                    } else{
+                        move_uploaded_file($_FILES["ann_image_url"]["tmp_name"], "media/annonce/" . $_FILES["ann_image_url"]["name"]);
+                        echo "Votre fichier a été téléchargé avec succès.";
+                    } 
+                } else{
+                    echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer."; 
+                }
+            } else{
+                echo "Error: " . $_FILES["ann_image_url"]["error"];
+            }
         }
+    }
+
+
     
 }
