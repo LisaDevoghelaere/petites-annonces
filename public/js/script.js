b16224e3a6ee37f17707e7e39b97b2ffd253b131
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-09-07 09:31:54 +0200

    mise en place tri par catégorie

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,32 @@
-const groupeAnnonce = document.querySelector('.groupe-annonce');
-const annonce = document.querySelectorAll('.annonce');
-//console.log(groupeAnnonce, infinite);
+const choisir = document.getElementById('choisir');
 
-function chargeAnnonces(numAnnonce = 10){
-    let i = 0;
-    while(i<numAnnonce){
-        groupeAnnonce.appendChild(annonce);
-        i++;
-    }
-}
-chargeAnnonces()
-window.addEventListener('scroll', () => {
-    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
-    chargeAnnonces()
+choisir.addEventListener('click', function(){
+    
+    //On sélectionne la valeur de la catégorie pour qu'elle corresponde à l'id de la table categorie
+    let select = document.getElementById('inputGroupSelect02').value;
+    //on créé un objet formdata qui créé un "formulaire"
+    let data = new FormData();
+    //on lui assigne des paramètres (nom, input select)
+    data.append('categorie', select);
+    
+    //la requête ajax
+    let xhr = new XMLHttpRequest();
+    //methode d'envoi, chemin, true
+    xhr.open('POST', 'choixcategorie', true);
+    //on envoi avec en valeur data, notre objet "formulaire"
+    xhr.send(data);
+    //où on en est dans les étapes et les fonction qu'on lui attribut
+    xhr.onreadystatechange = function (){
+        //cf : "XMLHTTPRequest status w3school"
+        if(this.readyState == 4 && this.status == 200){
+            content[0].innerHTML = xhr.responseText;
+        }
     }
 })
+
+// chargeAnnonces()
+// window.addEventListener('scroll', () => {
+//     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+//     chargeAnnonces()
+//     }
+// })
