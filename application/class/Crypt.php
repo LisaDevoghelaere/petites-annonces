1d14ae4d2d7dae8cbdff6ec44c35a6a64714395b
sawkea <shoareau@orange.fr>
2020-09-08 16:42:59 +0200

    Updates du projets

--- application/class/Crypt.php
+++ application/class/Crypt.php
@@ -4,25 +4,59 @@ namespace App;
 
 class Crypt {
 
-    public static $encryption_key = "a4g8r5hhmolj8r1ji";
-    public static $cipher ="aes-128-cbc";
-
-    public static function encrypt($string){
-
-        $options = 0;
-        $ivlen = openssl_cipher_iv_length((self::$cipher));
-
+    public static function encrypt_decrypt($action, $string) {
+        $output = false;
+    
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'a4g8r5hhmolj8r1ji';
         $characters = "abcdefghijklmnopkrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $secret_iv = $characters[random_int(0,51)];
         $iv = "";
+        $ivlen = openssl_cipher_iv_length(($encrypt_method));
         for($i=0; $i<$ivlen; $i++){
-            $iv = $characters[random_int(0,51)];
+                 $iv = $iv. $characters[random_int(0,51)];
+            }
+    
+        // hash
+        $key = hash('sha256', $secret_key);
+        
+        // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    
+        if ( $action == 'encrypt' ) {
+            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+            $output = base64_encode($output);
+        } else if( $action == 'decrypt' ) {
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
         }
-
-        $cipher_text = openssl_encrypt($string,self::$cipher,self::$encryption_key,$options,$iv);
-
-        return $iv.$cipher_text;
-
+    
+        return $output;
     }
+
+
+
+
+
+
+    // public static $encryption_key = "a4g8r5hhmolj8r1ji";
+    // public static $cipher ="aes-128-cbc";
+
+    // public static function encrypt($string){
+
+    //     $options = 0;
+    //     $ivlen = openssl_cipher_iv_length((self::$cipher));
+
+    //     $characters = "abcdefghijklmnopkrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //     $iv = "";
+    //     for($i=0; $i<$ivlen; $i++){
+    //         $iv = $characters[random_int(0,51)];
+    //     }
+
+    //     $cipher_text = openssl_encrypt($string,self::$cipher,self::$encryption_key,$options,$iv);
+
+    //     return $iv.$cipher_text;
+
+    // }
 
 
     // public static function decrypt($cipher_string){
@@ -35,12 +69,12 @@ class Crypt {
     //     return $text;
     // }
 
-    public static function hashCode($mail , $id){
-        $concat = (self::encrypt($mail)).(self::encrypt($id));
-        $code = hash('sha1',(urlencode($concat)));
+    // public static function hashCode($mail , $id){
+    //     $concat = (self::encrypt($mail)).(self::encrypt($id));
+    //     $code = hash('sha1',(urlencode($concat)));
 
-        return $code;
-    }
+    //     return $code;
+    // }
 
 
 }
