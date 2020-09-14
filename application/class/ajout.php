97ae10ad4f1656cff281a86728f52cedc187791d
sawkea <shoareau@orange.fr>
2020-09-14 11:25:06 +0200

    updates captcha partie php

--- application/class/ajout.php
+++ application/class/ajout.php
@@ -233,6 +233,27 @@ class Ajout{
 
 
         public function Validation(){
+
+            // Captcha
+            // Ma clé privée
+            $secret = "private_key";
+            // Paramètre renvoyé par le recaptcha
+            $response = $_POST['g-recaptcha-response'];
+            // On récupère l'IP de l'utilisateur
+            $remoteip = $_SERVER['REMOTE_ADDR'];
+
+            $api_url = "https://www.google.com/recaptcha/api/siteverify?secret="
+            . $secret
+            . "&response=" . $response
+            . "&remoteip=" . $remoteip ;
+
+            $decode = json_decode(file_get_contents($api_url), true);
+
+            if($decode['success'] == false){
+            return 'CAPTCHA Invalide';
+            }
+
+            
             if(isset($_POST['ann_titre']) && !empty($_POST['ann_titre'])){
                 if(v::stringVal()->validate($_POST['ann_titre']) == false){
                     return 'Titre invalide';
